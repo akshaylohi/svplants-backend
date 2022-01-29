c4acd4e24137f4cd55e27f53886b7d78f11eb964
akshaylohi <akshaylohi3@gmail.com>
2022-01-28 23:35:54 -0500

    adding dbscript

---
+++ SVPlantApi/DBScript/dbo.Plants.data.sql
@@ -0,0 +1,7 @@
+SET IDENTITY_INSERT [dbo].[Plants] ON
+INSERT INTO [dbo].[Plants] ([PlantId], [Status], [Name], [LastWateredTime]) VALUES (1, N'ok', N'Plant 1', N'2022-01-28 22:39:11')
+INSERT INTO [dbo].[Plants] ([PlantId], [Status], [Name], [LastWateredTime]) VALUES (3, N'ok', N'Plant 2', N'2022-01-28 22:38:14')
+INSERT INTO [dbo].[Plants] ([PlantId], [Status], [Name], [LastWateredTime]) VALUES (4, N'ok', N'Plant 3', N'2022-01-28 17:58:20')
+INSERT INTO [dbo].[Plants] ([PlantId], [Status], [Name], [LastWateredTime]) VALUES (5, N'ok', N'Plant 4', N'2022-01-28 17:58:23')
+INSERT INTO [dbo].[Plants] ([PlantId], [Status], [Name], [LastWateredTime]) VALUES (6, N'ok', N'Plant 5', N'2022-01-28 18:04:48')
+SET IDENTITY_INSERT [dbo].[Plants] OFF
